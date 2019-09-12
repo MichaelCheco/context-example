f911946460e8a640f92c7f9b83d6f209b7064f81
Michael <checomichael2@gmail.com>
2019-09-12 13:34:26 -0400

    working example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+const CountStateContext = React.createContext()
+const CountDispatchContext = React.createContext()
+function countReducer(state, action) {
+	switch (action.type) {
+		case 'ADD': {
+			return { count: state.count + 1 }
+		}
+		default: {
+			throw new Error()
+		}
+	}
+}
+export function CountProvider(props) {
+	const [state, dispatch] = React.useReducer(countReducer, { count: 0 })
+	return (
+		<CountStateContext.Provider value={state}>
+			<CountDispatchContext.Provider value={dispatch}>
+				{props.children}
+			</CountDispatchContext.Provider>
+		</CountStateContext.Provider>
+	)
+}
+function useCountState() {
+	const context = React.useContext(CountStateContext)
+	if (context === undefined) {
+		throw new Error('useCountState must be used within a CountProvider')
+	}
+	return context
+}
+function useCountDispatch() {
+	const context = React.useContext(CountDispatchContext)
+	if (context === undefined) {
+		throw new Error('useCountDispatch must be used within a CountProvider')
+	}
+	return context
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const state = useCountState()
+	const dispatch = useCountDispatch()
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>{state.count}</h1>
+				<button onClick={() => dispatch({ type: 'ADD' })}>increment</button>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
